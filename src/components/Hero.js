a1c6a9601395786175ea3031eade18f444d2610e
Ba6mala <basmalamohamed01013@gmail.com>
2025-02-10 14:38:45 +0200

    added Services

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaTwitter } from "react-icons/fa";
+// import { FaTwitter } from "react-icons/fa";
 function Hero(){
     return(
         <div className="hero">
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,27 @@
 function Services(){
     return(
-<div>
-    <h1>
-    
-    </h1>
-</div>
+      <div class="services">
+        <div class="container">
+            <button>SERVICES</button>
+            <div class="boxs">
+                <div class="box">
+                    <i class="fa-solid fa-pen-clip"></i>
+                    <h3>script writing</h3>
+                    <p>Lorem ipsum dolor all aman, comsanier add rww alite Reem ipsum doka amaration gusto saluptuary consacrator deleles aligned eum acusas recшни doll, alias disfindion illume expedita e masima, magna dolor providant</p>
+                </div>
+                <div class="box">
+                    <i class="fa-brands fa-chrome"></i>
+                    <h3>make websites</h3>
+                    <p>Lorem ipsum dolor all aman, comsanier add rww alite Reem ipsum doka amaration gusto saluptuary consacrator deleles aligned eum acusas recшни doll, alias disfindion illume expedita e masima, magna dolor providant</p>
+                </div>
+                <div class="box">
+                    <i class="fa-solid fa-video"></i>
+                    <h3>video production</h3>
+                    <p>Lorem ipsum dolor all aman, comsanier add rww alite Reem ipsum doka amaration gusto saluptuary consacrator deleles aligned eum acusas recшни doll, alias disfindion illume expedita e masima, magna dolor providant</p>
+                </div>
+            </div>
+        </div>
+     </div>
     );
 }
 export default Services;
