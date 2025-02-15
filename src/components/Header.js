68cfab07154e0b8f7f5bef8bbde1ef59b176198e
manona25 <manoshamedhat25@gmail.com>
2025-02-16 01:38:29 +0200

    modifies files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Header(){
-    return(
-<div className="header">
-<div className="container">
-<div className="logo">
-<a href="./Header.js"> Mo </a>
-</div>
-<nav> 
-    <ul> 
-        <li><a href="./Header.js"> Contact</a></li>
-        <li><a href="./Header.js"> Projects</a></li>
-        <li><a href="./Header.js"> Services</a></li>
-    </ul>
-</nav>
-</div>
-</div>
-    );
+function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="logo">
+          <a href="/mo" className="mo">MO</a>
+        </div>
+        <div className="burger-menu" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </div>
+        <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+          <li>
+            <a href="#home" onClick={() => setMenuOpen(false)}>Home</a>
+          </li>
+          <li>
+            <a href="#services" onClick={() => setMenuOpen(false)}>Services</a>
+          </li>
+          <li>
+            <a href="#projects" onClick={() => setMenuOpen(false)}>Projects</a>
+          </li>
+          <li>
+            <a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
+
 export default Header;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,22 +1,22 @@
 import React from "react";
 function Services(){
     return(
-    <div class="services">
-        <div class="container">
+    <div className="services">
+        <div className="container">
             <button>SERVICES</button>
-            <div class="boxs">
-                <div class="box">
-                    <i class="fa-solid fa-pen-clip"></i>
+            <div className="boxs">
+                <div className="box">
+                    <i className="fa-solid fa-pen-clip"></i>
                     <h3>script writing</h3>
                     <p>Lorem ipsum dolor all aman, comsanier add rww alite Reem ipsum doka amaration gusto saluptuary consacrator deleles aligned eum acusas recшни doll, alias disfindion illume expedita e masima, magna dolor providant</p>
                 </div>
-                <div class="box">
-                    <i class="fa-brands fa-chrome"></i>
+                <div className="box">
+                    <i className="fa-brands fa-chrome"></i>
                     <h3>make websites</h3>
                     <p>Lorem ipsum dolor all aman, comsanier add rww alite Reem ipsum doka amaration gusto saluptuary consacrator deleles aligned eum acusas recшни doll, alias disfindion illume expedita e masima, magna dolor providant</p>
                 </div>
-                <div class="box">
-                    <i class="fa-solid fa-video"></i>
+                <div className="box">
+                    <i className="fa-solid fa-video"></i>
                     <h3>video production</h3>
                     <p>Lorem ipsum dolor all aman, comsanier add rww alite Reem ipsum doka amaration gusto saluptuary consacrator deleles aligned eum acusas recшни doll, alias disfindion illume expedita e masima, magna dolor providant</p>
                 </div>
